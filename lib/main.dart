a0acafc80960ecacdacf350821f4a3d2c58a07eb
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-28 20:39:41 +0200

    console_math_quiz: for loop example - commit #4

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,21 @@ double promptDouble() {
   return myNumber;
 }
 
+void triangle(int numTriangle) {
+  for (int i = 0; i < numTriangle; i++){
+    print('   /|');
+    print('  / |');
+    print(' /  |');
+    print('/___|');
+  }
+}
+
 void main() {
   List<String> friends = ['Joe', 'Joey', 'Joseph'];
 
   for (String friend in friends) {
     print(friend.toUpperCase());
   }
+
+  triangle(5);
 }
